c2fb2bebc94e62f55ca33ce13c8b5360b94e2a92
Tien Pham <r3dsky02@yahoo.com.vn>
2019-01-19 23:05:55 +0200

    Post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 const port = 3000
@@ -12,6 +13,9 @@ const users = [
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
   res.render("index", {
     name: "Tien"
@@ -22,6 +26,15 @@ app.get('/users', (req, res) => {
   res.render("users/index", {
     users
   })
+})
+
+app.get('/users/create', (req, res) => {
+  res.render("users/create")
+})
+
+app.post('/users/create', (req, res) => {
+  users.push(req.body)
+  res.redirect('/users')
 })
 
 app.get('/users/search', (req, res) => {
