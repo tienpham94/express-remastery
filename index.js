4b411e683de193292469d0d4b55435f63c86a8d7
Tien Pham <r3dsky02@yahoo.com.vn>
2019-01-19 22:53:12 +0200

    Filter based on query

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 
 const port = 3000
 
+const users = [
+  {id: 1, name: "no1"},
+  {id: 2, name: "no2"},
+  {id: 2, name: "tiendzai"},
+]
+
 app.set('view engine', 'pug')
 app.set('views', './views')
 
@@ -14,10 +20,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
   res.render("users/index", {
-    users: [
-      {id: 1, name: "no1"},
-      {id: 2, name: "no2"}
-    ]
+    users
+  })
+})
+
+app.get('/users/search', (req, res) => {
+  const {q} = req.query
+  const matchedUsers = users.filter(u => u.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+  res.render("users/index", {
+    users: matchedUsers
   })
 })
 
