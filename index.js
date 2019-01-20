df71f97b600b7c61cf6aa4449a9d668a8a7d9c32
Tien Pham <r3dsky02@yahoo.com.vn>
2019-01-20 12:54:13 +0200

    Router

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const shortid = require('shortid')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
 const app = express()
-const adapter = new FileSync('db.json')
-const db = low(adapter)
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write()
 
 const port = 3000
+const userRoute = require('./routes/user')
 
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -25,34 +17,6 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/users', (req, res) => {
-  res.render("users/index", {
-    users: db.get('users').value()
-  })
-})
-
-app.get('/users/create', (req, res) => {
-  res.render("users/create")
-})
-
-app.post('/users/create', (req, res) => {
-  req.body.id = shortid.generate()
-  db.get("users").push(req.body).write()
-  res.redirect('/users')
-})
-
-app.get('/users/:id', (req, res) => {
-  const {id} = req.params
-  const user = db.get('users').find({id}).value()
-  res.render("users/view", { user })
-})
-
-app.get('/users/search', (req, res) => {
-  const {q} = req.query
-  const matchedUsers = users.filter(u => u.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
-  res.render("users/index", {
-    users: matchedUsers
-  })
-})
+app.use('/users', userRoute)
 
 app.listen(port, () => console.log(port))
---
+++ routes/user.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const shortid = require('shortid')
+
+const db = require('../db')
+
+router.get('/', (req, res) => {
+  res.render("users/index", {
+    users: db.get('users').value()
+  })
+})
+
+router.get('/create', (req, res) => {
+  res.render("users/create")
+})
+
+router.post('/create', (req, res) => {
+  req.body.id = shortid.generate()
+  db.get("users").push(req.body).write()
+  res.redirect('/users')
+})
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params
+  const user = db.get('users').find({id}).value()
+  res.render("users/view", { user })
+})
+
+router.get('/search', (req, res) => {
+  const {q} = req.query
+  const matchedUsers = db.get('users').value().filter(u => u.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+  res.render("users/index", {
+    users: matchedUsers
+  })
+})
+
+module.exports = router
